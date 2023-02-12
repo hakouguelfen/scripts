863102fe1f128d373d075f9d62fa1b4f90bad9dc
hakou guelfen <hakoudev@gmail.com>
2023-02-12 16:21:16 +0100

    feat: add gtihub.sh to find all my repos

---
+++ github.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+main () {
+    github="https://github.com/"
+    repos=$(gh repo list |
+                awk '{
+                        if ($(NF-1) == "fork") printf("%-30s %-20s\n", $1, $(NF-2))
+                        else printf("%-30s %-20s\n", $1, $(NF-1))
+                    }' |
+                sed 's/hakouklvn\///' |
+                sed 's/,//' |
+                sort
+         )
+
+    repo=$(printf "%s\n" "${repos}" | dmenu -i -p 'Github repositories: ') || exit 1;
+    link=$(awk '{print $1}' <<< "${github}${repo}")
+
+    [ "$repo" ] && xdg-open "$link"
+}
+
+
+main "$@"
